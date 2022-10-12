9e0ade905d8c436dda7291675144c09deb503e9d
Ferran Cañellas <ferran.canellas@i2cat.net>
2022-10-12 18:41:40 +0200

    Add custom keymaps for corne and keychron

---
+++ keyboards/crkbd/keymaps/ferrancanellas/rules.mk
@@ -0,0 +1,18 @@
+# RBG
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE  = no
+
+# OLED
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+
+# TAP_DANCE
+TAP_DANCE_ENABLE = yes
+
+# Not used
+MOUSEKEY_ENABLE = no       # Mouse keys
+
+# EXTRAS
+LTO_ENABLE      = yes      # Enable link time optimization to reduce binary size
+# EXTRAKEY_ENABLE = yes      # Audio control and System control
+# KEY_LOCK_ENABLE = yes
